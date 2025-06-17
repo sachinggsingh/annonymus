d1b5e3d6693f3a97eca4fa48286126b651150938
Sachin Singh <152975718+sachinggsingh@users.noreply.github.com>
2025-06-17 13:57:06 +0530

    cleared the log and logger

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -9,7 +9,7 @@ export const logger =  winston.createLogger({
     new winston.transports.File({ filename: 'combined.log' }),
     ],
 })
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== 'development') {
     logger.add(new winston.transports.Console({
     format: winston.format.simple(),
     }));
