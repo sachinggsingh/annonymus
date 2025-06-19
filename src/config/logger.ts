4d506ec137e85068b823e2c585d5c1e63d33fb23
Sachin Singh <152975718+sachinggsingh@users.noreply.github.com>
2025-06-19 15:48:43 +0530

    order.ts

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -4,10 +4,6 @@ export const logger =  winston.createLogger({
     level: 'info',
     format: winston.format.json(),
     defaultMeta: { service: 'service' },
-    transports: [
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' }),
-    ],
 })
 if (process.env.NODE_ENV !== 'development') {
     logger.add(new winston.transports.Console({
